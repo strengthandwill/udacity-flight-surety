4ef7e17e529ed55b6128c7c39c205eaf40a1aec6
poh_kah_kong <poh_kah_kong@tech.gov.sg>
2021-10-11 20:44:43 +0800

    Clean up code

--- src/dapp/index.js
+++ src/dapp/index.js
@@ -6,23 +6,12 @@ import './flightsurety.css';
 const NULL_ADDRESS = "0x0000000000000000000000000000000000000000";
 
 (async() => {
-
-    let result = null;
-
     let contract = new Contract('localhost', () => {
-
-        // Read transaction
-        // contract.isOperational((error, result) => {
-        //     display('Operational Status', 'Check if contract is operational', [ { label: 'Operational Status', error: error, value: result} ]);
-        // }); 
-
         checkLoginAccount(contract);
 
         loadAirlines(contract);
         loadFlights(contract);
         loadInsurances(contract);
-        // setTimeout(() => {  loadFlights(contract) }, 100);        
-        // setTimeout(() => {  loadInsurances(contract) }, 200);
 
         loadAirlineActions(contract);   
         loadFlightActions(contract);
@@ -204,20 +193,6 @@ function loadInsurances(contract) {
             insureesTable.appendChild(tr);
         }
     });
-}    
-
-function display(title, description, results) {
-    let displayDiv = DOM.elid("display-wrapper");
-    let section = DOM.section();
-    section.appendChild(DOM.h2(title));
-    section.appendChild(DOM.h5(description));
-    results.map((result) => {
-        let row = section.appendChild(DOM.div({className:'row'}));
-        row.appendChild(DOM.div({className: 'col-sm-4 field'}, result.label));
-        row.appendChild(DOM.div({className: 'col-sm-8 field-value'}, result.error ? String(result.error) : String(result.value)));
-        section.appendChild(row);
-    })
-    displayDiv.append(section);
 }
 
 
--- src/server/server.js
+++ src/server/server.js
@@ -20,53 +20,57 @@ const STATUS_CODE_LATE_OTHER = 50;
 web3.eth.defaultAccount = web3.eth.accounts[0];
 let flightSuretyApp = new web3.eth.Contract(FlightSuretyApp.abi, config.appAddress);
 
-
-flightSuretyApp.events.OracleRequest({
-    fromBlock: 0
-  }, async (error, event) => {    
-    const accounts = await web3.eth.getAccounts();    
-    for (let a = 21; a <= ORACLES_COUNT + 20; a++) {
-      // Get oracle information          
-      let oracleIndexes = await flightSuretyApp.methods.getMyIndexes().call({ from: accounts[a] });      
-      for (let idx = 0; idx < 3; idx++) {
-        try {
-          // Submit a response...it will only be accepted if there is an Index match
-          let statusCode = STATUS_CODE_LATE_AIRLINE;
-          // let statusCode = parseInt(Math.random() * 6) * 10;
-          await flightSuretyApp.methods.submitOracleResponse(
-            oracleIndexes[idx], 
-            event.returnValues.airline, 
-            event.returnValues.flight, 
-            event.returnValues.timestamp, 
-            statusCode
-          ).send ({ from: accounts[a] });
-        }        
-        catch (e) {
-          // Enable this when debugging
-          // console.log('\nError', idx, oracleIndexes[idx], event.returnValues.airline, event.returnValues.flight, event.returnValues.timestamp);
-        }
-      }
-    }
-});
-
-// flightSuretyApp.getPastEvents('OracleRegistered', {fromBlock: 0, toBLock: 'latest'}, (error, events) => {
-//   for (let event of events) {
-//     console.log(event);
-//   }
-// });
+let oracles = [];
 
 (async function() {    
   const accounts = await web3.eth.getAccounts();
   for (let a = 21; a <= ORACLES_COUNT + 20; a++) {
-    let result
     try {      
-      // console.log(accounts[a]);    
-      await flightSuretyApp.methods.registerOracle().send({ from: accounts[a], value: ONE_ETHER, gas: 6000000 });                
+      await flightSuretyApp.methods.registerOracle().send({ from: accounts[a], value: ONE_ETHER, gas: 6000000 });
+      let indexes = await flightSuretyApp.methods.getMyIndexes().call({ from: accounts[a] });                      
+      oracles.push({
+        oracle: accounts[a],
+        indexes: indexes
+      })
     } catch (e) {
-      // console.log(e);
+      console.log(e);
      }                    
   }
 })();
+
+flightSuretyApp.events.OracleRequest({
+  fromBlock: 0
+}, async (error, event) => {    
+
+  for (let oracle of oracles) {   
+  // for (let a = 21; a <= ORACLES_COUNT + 20; a++) {
+    // Get oracle information   
+    let indexes = oracle.indexes;
+    // let oracleIndexes = await flightSuretyApp.methods.getMyIndexes().call({ from: accounts[a] });      
+    for (let idx = 0; idx < 3; idx++) {
+      let airline = event.returnValues.airline;
+      let flight = event.returnValues.flight;
+      let timestamp = event.returnValues.timestamp;
+      let statusCode = parseInt(Math.random() * 6) * 10;
+      try {
+        // Submit a response...it will only be accepted if there is an Index match
+        // let statusCode = STATUS_CODE_LATE_AIRLINE;      
+        await flightSuretyApp.methods.submitOracleResponse(
+          indexes[idx], 
+          airline, 
+          flight, 
+          timestamp, 
+          statusCode
+        ).send ({ from: oracle.oracle });
+        console.log('\Success', idx, indexes[idx], airline, flight, timestamp, statusCode);
+      }              
+      catch (e) {
+        // Enable this when debugging
+        console.log('\nError', idx, indexes[idx], airline, flight, timestamp, e.message);
+      }
+    }
+  }
+});
 
 const app = express();
 app.get('/api', (req, res) => {
